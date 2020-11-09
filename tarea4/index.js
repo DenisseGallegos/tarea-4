24dc360715a496c8311e61aa419f624d21aa2eed
DenisseGallegos <72174324+DenisseGallegos@users.noreply.github.com>
2020-11-08 22:46:45 -0600

    Add files via upload

---
+++ tarea4/index.js
@@ -0,0 +1,12 @@
+var  speakWord  =  "Adiós" ;
+
+function  speak( nombre )  {
+  consola . log ( speakWord  +  ""  +  nombre ) ;
+}
+
+var  speakWord  =  "Adiós" ;
+
+function speak( nombre )  {
+  consola . log ( speakWord  +  ""  +  nombre ) ;
+}
+
